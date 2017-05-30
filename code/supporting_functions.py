cecdb04fdce87ce71b2a6123b41ac0b181640649
iFoxbox <foxboxmw@gmail.com>
2017-05-30 19:06:44 -0400

    Fixed the Supportin_function.py bug

--- code/supporting_functions.py
+++ code/supporting_functions.py
@@ -43,16 +43,16 @@ def update_rover(Rover, data):
       Rover.samples_found = Rover.samples_to_find - np.int(data["sample_count"])
 
       print('speed =',Rover.vel, 'position =', Rover.pos, 'throttle =', 
-<<<<<<< HEAD
-      Rover.throttle, 'steer_angle =', Rover.steer, 'near_sample', Rover.near_sample, 
-      'picking_up', data["picking_up"])
+#<<<<<<< HEAD
+#      Rover.throttle, 'steer_angle =', Rover.steer, 'near_sample', Rover.near_sample, 
+ #     'picking_up', data["picking_up"])
 
-=======
+#=======
       Rover.throttle, 'steer_angle =', Rover.steer, 'near_sample:', Rover.near_sample, 
       'picking_up:', data["picking_up"], 'sending pickup:', Rover.send_pickup, 
       'total time:', Rover.total_time, 'samples remaining:', data["sample_count"], 
       'samples found:', Rover.samples_found)
->>>>>>> refs/remotes/udacity/master
+#>>>>>>> refs/remotes/udacity/master
       # Get the current image from the center camera of the rover
       imgString = data["image"]
       image = Image.open(BytesIO(base64.b64decode(imgString)))
