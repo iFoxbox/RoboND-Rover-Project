1a09cd07456cd0597c12507abc1a8fba8fb1f83f
iFoxbox <foxboxmw@gmail.com>
2017-05-30 18:41:18 -0400

    Added some steering and a better way of turning around

--- code/decision.py
+++ code/decision.py
@@ -1,6 +1,20 @@
 import numpy as np
 
 
+#create a reusable stop function
+def trigger_stop(Rover): 
+    # Set mode to "stop" and hit the brakes!
+    Rover.throttle = 0
+    # Set brake to stored brake value
+    Rover.brake = Rover.brake_set
+    Rover.steer = 0
+    Rover.mode = 'stop'
+    
+#function will be called to decide where to turn to
+def adjust_wheel(Rover):
+    # Set steering to average angle clipped to the range +/- 15
+    Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
+    
 # This is where you can build a decision tree for determining throttle, brake and steer 
 # commands based on the output of the perception_step() function
 def decision_step(Rover):
@@ -23,19 +37,14 @@ def decision_step(Rover):
                     Rover.throttle = Rover.throttle_set
                 else: # Else coast
                     Rover.throttle = 0
+                    
                 Rover.brake = 0
-           
-                # Set steering to average angle clipped to the range +/- 15
-                Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi - 5), -15, 15)
+                adjust_wheel(Rover)
                     
             # If there's a lack of navigable terrain pixels then go to 'stop' mode
             elif (len(Rover.nav_angles) < Rover.stop_forward) or Rover.near_sample:
-                    # Set mode to "stop" and hit the brakes!
-                    Rover.throttle = 0
-                    # Set brake to stored brake value
-                    Rover.brake = Rover.brake_set
-                    Rover.steer = 0
-                    Rover.mode = 'stop'
+                trigger_stop(Rover)
+                
             
                 
         # If we're already in "stop" mode then make different decisions
@@ -48,13 +57,18 @@ def decision_step(Rover):
             # If we're not moving (vel < 0.2) then do something else
             elif Rover.vel <= 0.2:
                 # Now we're stopped and we have vision data to see if there's a path forward
-                if len(Rover.nav_angles) < Rover.go_forward:
+                if (len(Rover.nav_angles) < Rover.go_forward) and (80 <= np.mean(Rover.nav_angles * 180/np.pi)<= 100 ):
                     Rover.throttle = 0
                     # Release the brake to allow turning
                     Rover.brake = 0
-                    # Turn range is +/- 15 degrees, when stopped the next line will induce 4-wheel turning
-                    Rover.steer = +15 # Could be more clever here about which way to turn
                     
+                    #try to get to 90 degrees 
+                    if np.mean(Rover.nav_angles * 180/np.pi) <90: 
+                        # Turn range is +/- 15 degrees, when stopped the next line will induce 4-wheel turning
+                        Rover.steer = +15 
+                    else: 
+                        Rover.steer = -15
+                        
                 #If a sample is pressent set grab flag 
                 if Rover.near_sample: 
                     Rover.send_pickup = True
